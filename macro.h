297d903a94d64c3bf7733733f772c6108364b122
Joshua Rowe <17525998+omnisci3nce@users.noreply.github.com>
2022-06-13 19:33:42 +1000

    Update macro.h

--- macro.h
+++ macro.h
@@ -3,7 +3,7 @@
 #define SQL_CALL_AND_CHECK(db, result) do { \
   int rc = (result); \
   if (rc != SQLITE_OK) { \
-    fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db)); \
+    fprintf(stderr, "SQL Error: %s\n", sqlite3_errmsg(db)); \
     sqlite3_close(db); \
     return 1; \
   } \
